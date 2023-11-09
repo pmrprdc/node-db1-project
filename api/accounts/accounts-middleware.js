dd8d85312c517b05baf8cb643ebbced10829cf2b
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-09 16:19:33 -0500

    added console logs inside middleware functions for debugging and sanity check

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,12 +2,18 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-}
+  console.log("checkAccountPauload initiated")
+  next();
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log("checkAccountNameUnique middleware inititated ")
+  next()
+  
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  console.log("checkAccountId initiated")
+  next();
 }
